d9c8a56cfefe7c5cce734153f33c8af149bf61c4
Anna Yasenova <yasenova2013@gmail.com>
2017-02-19 21:16:59 +0200

    Using Map. Testing with different data

--- JavaScript/2-speed.js
+++ JavaScript/2-speed.js
@@ -3,14 +3,15 @@
 const LOOP = 10000;
 
 function memoize(fn) {
-  const cache = {};
+  const cache = new Map();
   return (...args) => {
     const key = args + '';
-    const val = cache[key];
-    if (val) return val;
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
     else {
       const res = fn(...args);
-      cache[key] = res;
+      cache.set(key, res);
       return res;
     }
   };
@@ -20,7 +21,8 @@ let fib = n => (n <= 2) ? 1 : fib(n - 1) + fib(n - 2);
 
 function speedTest(name, fn, args, count) {
   let start = new Date().getTime();
-  for (let i = 0; i < count; i++) {
+  let i; //or use "var" in for(...). (no)
+  for (i = 0; i < count; i++) {
     fn(...args);
   }
   let end = new Date().getTime();
@@ -28,6 +30,12 @@ function speedTest(name, fn, args, count) {
   console.log(`${name} * ${count} : ${time}`);
 }
 
+speedTest('fib(10)', fib, [10], LOOP);
+speedTest('fib(15)', fib, [15], LOOP);
 speedTest('fib(20)', fib, [20], LOOP);
+speedTest('fib(25)', fib, [25], LOOP);
 fib = memoize(fib);
+speedTest('memoized fib(10)', fib, [10], LOOP);
+speedTest('memoized fib(15)', fib, [15], LOOP);
 speedTest('memoized fib(20)', fib, [20], LOOP);
+speedTest('memoized fib(25)', fib, [25], LOOP);
