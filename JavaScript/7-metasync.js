76128ca542c26ab48a5ba860e162845e59a82427
tshemsedinov <timur.shemsedinov@gmail.com>
2018-06-11 10:02:34 +0300

    Add example with metasync dependency

---
+++ JavaScript/7-metasync.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const fs = require('fs');
+const metasync = require('metasync');
+
+// Usage
+
+fs.readFile = metasync.memoize(fs.readFile);
+
+fs.readFile('6-metasync.js', (err, data) => {
+  console.log('data length: ' + data.length);
+  fs.readFile('6-metasync.js', (err, data) => {
+    console.log('data length: ' + data.length);
+  });
+});
