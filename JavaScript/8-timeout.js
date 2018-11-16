7367d48fd38e51280d4e80883883091f7b322989
ALEGATOR1209 <alekos1209@gmail.com>
2018-11-16 21:06:20 +0200

    Fixed small problems

--- JavaScript/8-timeout.js
+++ JavaScript/8-timeout.js
@@ -2,34 +2,41 @@
 
 const memoize = (fn, msec = 100) => {
   let cache = {};
-  let timer = setTimeout(() => {
-    console.log('Cache cleaned');
-    timer = null;
-    cache = {};
-  }, msec);
 
   const setTimer = msec => {
-    timer = setTimeout(() => {
+    let timer = setTimeout(() => {
       console.log('Cache cleaned');
       timer = null;
       cache = {};
     }, msec);
+
+    return timer;
   };
 
-  const generateKey = (...args) => args.join('|');
+  const hasKey = (key) => Object.keys(cache).includes(key);
+
+  const timer = setTimer(msec);
+
+  const generateKey = (args) => {
+    let key = '';
+    for (const arg of args)
+      key += `${arg}~${typeof arg}|`;
+    return key;
+  };
 
   return (...args) => {
     if (!timer) setTimer(msec);
-    const key = generateKey(...args);
+    const key = generateKey(args);
 
-    if (cache[key]) {
+    if (hasKey(key)) {
       console.log(`From cache: ${args} = ${cache[key]}`);
       return cache[key];
     }
 
     console.log(`Calculate: ${args} = ${fn(...args)}`);
     const res = fn(...args);
-    cache[key] = res;
+    if (res !== undefined)
+      cache[key] = res;
     console.log(cache);
     return res;
   };
@@ -40,8 +47,8 @@ const sum = (a, b) => a + b;
 const sumM = memoize(sum, 2000);
 
 //USAGE
-sumM(1, 2);
-sumM(1, 2);
+sumM(1, -1);
+sumM(1, -1);
 setTimeout(() => {
-  sumM(1, 2);
+  sumM(1, -1);
 }, 2500);
