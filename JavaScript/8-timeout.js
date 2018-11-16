c4a9f226da2cc446f82ac02c4f33c9802cc218e1
ALEGATOR1209 <alekos1209@gmail.com>
2018-11-16 23:50:46 +0200

    timeout now deletes only keys which weren't used for some time

--- JavaScript/8-timeout.js
+++ JavaScript/8-timeout.js
@@ -1,22 +1,9 @@
 'use strict';
 
 const memoize = (fn, msec = 100) => {
-  let cache = {};
-
-  const setTimer = msec => {
-    let timer = setTimeout(() => {
-      console.log('Cache cleaned');
-      timer = null;
-      cache = {};
-    }, msec);
-
-    return timer;
-  };
-
+  const cache = {};
+  let timer;
   const hasKey = (key) => Object.keys(cache).includes(key);
-
-  const timer = setTimer(msec);
-
   const generateKey = (args) => {
     let key = '';
     for (const arg of args)
@@ -24,31 +11,67 @@ const memoize = (fn, msec = 100) => {
     return key;
   };
 
-  return (...args) => {
-    if (!timer) setTimer(msec);
+  const throwGarbage = () => {
+    const cleaningTime = new Date();
+    const removingTime = cleaningTime - msec;
+
+    const toDelete = Object.keys(cache)
+      .filter(key => cache[key].lastUse >= removingTime);
+
+    for (const key in cache) {
+      if (toDelete.includes(key)) {
+        console.log(`${key} deleted.`);
+        delete cache[key];
+      }
+      if (Object.keys(cache).length === 0) {
+        console.log('Cache is empty');
+        clearInterval(timer);
+        timer = null;
+      }
+    }
+  };
+
+  const setTimer = (msec) => {
+    const timer = setInterval(() => throwGarbage(), msec);
+    return timer;
+  };
+
+  timer = setTimer(msec);
+
+  const func = (...args) => {
+    if (!timer) timer = setTimer(msec);
     const key = generateKey(args);
 
     if (hasKey(key)) {
-      console.log(`From cache: ${args} = ${cache[key]}`);
-      return cache[key];
+      console.log(`From cache: ${args} = ${cache[key].value}`);
+      cache[key].lastUse = new Date();
+      console.log(cache);
+      return cache[key].value;
     }
 
     console.log(`Calculate: ${args} = ${fn(...args)}`);
     const res = fn(...args);
     if (res !== undefined)
-      cache[key] = res;
+      cache[key] = { value: res, lastUse: new Date() };
     console.log(cache);
     return res;
   };
+
+  return func;
 };
 
 const sum = (a, b) => a + b;
 
-const sumM = memoize(sum, 2000);
+const sumM = memoize(sum, 250);
 
 //USAGE
 sumM(1, -1);
-sumM(1, -1);
+sumM(2, -1);
 setTimeout(() => {
   sumM(1, -1);
-}, 2500);
+  setTimeout(() => sumM(2, -1), 100);
+  setTimeout(() => {
+    sumM(2, 3);
+    setTimeout(() => sumM(3, 4), 1000);
+  }, 500);
+}, 200);
