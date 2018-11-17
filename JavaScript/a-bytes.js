d19d32683108b9495454c25d8a709d38b675fadd
ALEGATOR1209 <alekos1209@gmail.com>
2018-11-17 10:52:44 +0200

    Improved a-bytes.js

--- JavaScript/a-bytes.js
+++ JavaScript/a-bytes.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const memoizeBytes = (fn, size) => {
-  let cache = {};
+const memoizeBytes = (fn, size = '1K', logging = true) => {
+  let cache = new Map();
 
   const generateKey = (...args) => args.join('|');
 
@@ -24,57 +24,53 @@ const memoizeBytes = (fn, size) => {
 
   const getSize = (obj) => {
     let bytes = 0;
-    for (const i in obj) {
-      switch (typeof obj[i]) {
-        case 'number':
-          bytes += 8;
-          break;
-        case 'boolean':
-          bytes += 4;
-          break;
-        case 'string':
-          bytes += obj[i].length * 2;
-          break;
-        case 'object':
-          bytes += getSize(obj[i]);
-          break;
-        default: throw new Error('getSize: Wrong field type.');
-      }
-    }
+    const dictTypes = {
+      number: () => 8,
+      boolean: () => 4,
+      string: (str) => str.length * 2,
+      object: (ob) => getSize(ob),
+    };
+    obj.forEach((value) => {
+      const getBytes = dictTypes[typeof value];
+      bytes += getBytes(value);
+    });
     return bytes;
   };
 
   return (...args) => {
     const key = generateKey(...args);
+    const record = cache.get(key);
 
-    if (cache[key]) {
-      console.log(`From cache: ${key}`);
+    if (record) {
+      if (logging) console.log(`From cache: ${key}`);
       return cache[key];
     }
 
-    if (getSize(cache) >= sizeInBytes) {
-      console.log(`Cache with size ${getSize(cache)} bytes cleaned`);
-      cache = {};
+    const sizeOfCache = getSize(cache);
+    if (sizeOfCache >= sizeInBytes) {
+      console.log(`Cache with size ${sizeOfCache} bytes cleaned`);
+      cache = new Map();
     }
 
-    console.log(`Calculating: ${key}`);
-    cache[key] = fn(...args);
-    console.dir(`Cache size: ${getSize(cache)}`);
-    return fn(...args);
+    if (logging) console.log(`Calculating: ${key}`);
+    const res = fn(...args);
+    cache.set(key, res);
+    if (logging) console.dir(`Cache size: ${sizeOfCache}`);
+    return res;
   };
 };
 
 //USAGE
 
 const sum = (a, b) => a + b;
-const sumB = memoizeBytes(sum, '32B');
-console.log(`sumB(1, 2) = ${sumB(1, 2)}`);
-console.log(`sumB(1, 2) = ${sumB(1, 2)}`);
-console.log(`sumB(2, 3) = ${sumB(2, 3)}`);
-console.log(`sumB(3, 2) = ${sumB(3, 2)}`);
-console.log(`sumB(4, 2) = ${sumB(4, 2)}`);
-console.log(`sumB(2, 3) = ${sumB(2, 3)}`);
-console.log(`sumB(1, 3) = ${sumB(1, 3)}`);
-console.log(`sumB(4, 3) = ${sumB(4, 3)}`);
-console.log(`sumB(9, 3) = ${sumB(1, 3)}`);
-console.log(`sumB(8, 3) = ${sumB(8, 3)}`);
+const sum1 = memoizeBytes(sum, '32B');
+console.log(`sum1(1, 2) = ${sum1(1, 2)}`);
+console.log(`sum1(1, 2) = ${sum1(1, 2)}`);
+console.log(`sum1(2, 3) = ${sum1(2, 3)}`);
+console.log(`sum1(3, 2) = ${sum1(3, 2)}`);
+console.log(`sum1(4, 2) = ${sum1(4, 2)}`);
+console.log(`sum1(2, 3) = ${sum1(2, 3)}`);
+console.log(`sum1(1, 3) = ${sum1(1, 3)}`);
+
+const sum2 = memoizeBytes(sum, '0.5K', false);
+for (let i = 0; i < 100; i++, sum2(i, i - 1));
